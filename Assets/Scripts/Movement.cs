bc03927ee0056a6f84ac2fc9248df6dd984427a8
huylq <huylq2603@gmail.com>
2024-01-27 18:34:48 +0900

    Improve player hit interaction

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public int health = 3;
+    private bool freezeControl = false;
     public float movementForce;
     public float movementDrag;
     public float dragtolerance = 0;
@@ -26,35 +28,72 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            lastPos = Input.mousePosition;
-
-
+        if (health <= 0) {
+            Debug.Log("Game Over");
         }
-        else if (Input.GetMouseButton(0))
+        if (!freezeControl)
         {
-            delta = Input.mousePosition - lastPos;
-            //           Debug.Log($"X: {delta.x}; Y: {delta.y}");
-            //         Debug.Log($"Direction: {delta.normalized}");
-            // Do Stuff here
+            if (Input.GetMouseButtonDown(0))
+            {
+                lastPos = Input.mousePosition;
 
-            r2d.AddRelativeForce(delta.normalized * movementForce);
 
-            // End do stuff
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                delta = Input.mousePosition - lastPos;
+                //           Debug.Log($"X: {delta.x}; Y: {delta.y}");
+                //         Debug.Log($"Direction: {delta.normalized}");
+                // Do Stuff here
 
-            lastPos = Input.mousePosition;
+                r2d.AddRelativeForce(delta.normalized * movementForce);
+
+                // End do stuff
+
+                lastPos = Input.mousePosition;
+            }
+            if (!Input.GetMouseButton(0) && r2d.velocity.magnitude > 0)
+            {
+                r2d.drag = 100f;
+            }
+            else
+            {
+                r2d.drag = movementDrag;
+            }
         }
-
-        if (!Input.GetMouseButton(0) && r2d.velocity.magnitude > 0)
-        {
-            r2d.drag = 100f;
-        }
-        else
-        {
-            r2d.drag = movementDrag;
-        }
-
         //Debug.Log($"Drag: {r2d.drag}");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject otherGameObject = other.gameObject;
+        string layer = LayerMask.LayerToName(otherGameObject.layer);
+        if (layer.Equals(StaticData.Layer.Obstacle))
+        {
+            health -= 1;
+            Debug.Log(health + " health");
+            r2d.drag = movementDrag;
+            r2d.velocity = Vector2.zero;
+            r2d.AddRelativeForce(Vector2.up * 100);
+            StartCoroutine(PlayerHit());
+        }
+    }
+
+    IEnumerator PlayerHit()
+    {
+        freezeControl = true;
+        Renderer rd = GetComponent<Renderer>();
+        rd.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        rd.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        rd.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        rd.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        rd.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        rd.enabled = true;
+        freezeControl = false;
     }
 }

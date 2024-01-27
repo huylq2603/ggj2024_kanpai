fd620d427323ed135b5e1e77f7f03c67d42f741b
huylq <huylq2603@gmail.com>
2024-01-27 13:42:27 +0900

    Add trigger

---
+++ Assets/Scripts/CatTriggerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatTriggerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Cat triggered");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/TriggerRange.cs
+++ Assets/Scripts/TriggerRange.cs
@@ -9,10 +9,11 @@ public class TriggerRange : MonoBehaviour
         GameObject otherGameObject = other.gameObject;
         string layer = LayerMask.LayerToName(otherGameObject.layer);
 
-        
         if (layer.Equals(StaticData.Layer.Player))
         {
             GameObject parent = gameObject.transform.parent.gameObject;
+            GameObject trigger = parent.transform.Find("Trigger").gameObject;
+            trigger.SetActive(true);
             Debug.Log("Hit " + parent);
             Debug.Log("Hit Player");
         }

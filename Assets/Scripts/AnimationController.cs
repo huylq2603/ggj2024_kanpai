8b0a3335375fafa99553aaa4282f649bbcfad5fd
huylq <huylq2603@gmail.com>
2024-01-28 14:21:45 +0900

    Sounds

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -7,6 +7,13 @@ public class AnimationController : MonoBehaviour
     public List<GameObject> frames;
     int currentFrame = 0;
 
+    public AudioClip naniSound;
+    
+    AudioSource audioSrc;
+
+    private void Awake() {
+        audioSrc = GetComponent<AudioSource>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -16,6 +23,9 @@ public class AnimationController : MonoBehaviour
             {
                 frames[currentFrame].SetActive(false);
                 frames[++currentFrame].SetActive(true);
+                if (currentFrame == 2) {
+                    audioSrc.PlayOneShot(naniSound);
+                }
             } else {
                 GameManager.LoadNextScene();
             }
